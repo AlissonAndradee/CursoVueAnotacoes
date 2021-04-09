17f17e1df8db92d8d202ee4bcc247e3382d58679
Alisson Andrade <alisson.piris@wdna.com.br>
2021-04-09 14:27:15 -0300

    Curso VUE Primeiros Passos

---
+++ src/components/Cliente.vue
@@ -0,0 +1,26 @@
+<template>
+    
+    <div id="cliente">
+        <h2 id="cliente-nome">Cliente aqui - Componente aqui</h2>
+        <h3> descrição do cliente: hahahahahahahhahahaha </h3>
+        <hr>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    #cliente{
+        color:blue;
+        background-color: black;
+    }
+
+    #cliente-nome{
+        color:blanchedalmond;
+    }
+</style>
---
+++ src/components/Produto.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3>Meu Produto</h3>
+        <p>bal balalallalalallalal</p>
+        <input type="text" placeholder="preco">
+    </div>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
